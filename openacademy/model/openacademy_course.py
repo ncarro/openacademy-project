51e3145b0cdefe367ada354cb2e4581bc9bc78fb
Nicoláas Carro <ncarro@sisinfo.com.uy>
2016-08-16 20:32:18 +0000

    [REF] openacademy: Adding relaccion entre sesion y curso

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -13,3 +13,6 @@ class Course(models.Model):
 
     name = fields.Char(string="Title", required=True)  # Field reserved
     description = fields.Text(string='Description')
+    responsible_id = fields.Many2one('res.users',
+                                      ondelete='set null', string="Responsible", index=True)
+
