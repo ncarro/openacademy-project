33fe8ab17f7010cd42cb9e75eaa4438271dec634
Nicoláas Carro <ncarro@sisinfo.com.uy>
2016-08-17 21:29:19 +0000

    [REF] openacademy: Add partner inherit

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -27,6 +27,7 @@
     'data': [
         'view/openacademy_course_view.xml',
         'view/openacademy_session_view.xml',
+        'view/partner.xml',
         # 'security/ir.model.access.csv',
         #'templates.xml',
     ],
--- openacademy/model/__init__.py
+++ openacademy/model/__init__.py
@@ -1,5 +1,6 @@
 
 from . import openacademy_course
 from . import openacademy_session
+from . import partner
 
 
