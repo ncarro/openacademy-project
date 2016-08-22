ee65067e8d3cf0d1780b17e932aee045973fd814
Nicoláas Carro <ncarro@sisinfo.com.uy>
2016-08-22 23:27:38 +0000

    [REF] openacademy: Upd WS caller

--- web_services/ws_test.py
+++ web_services/ws_test.py
@@ -20,13 +20,21 @@ model = 'openacademy.session'
 domain = []
 method_name = 'search_read'
 sessions = call(model, method_name, domain, ['name','seats','taken_seats'])
-for session in sessions:
-    print "Session %s (%s seats), taken seats %d" % (session['name'], session['seats'], session['taken_seats'])
+#for session in sessions:
+#    print "Session %s (%s seats), taken seats %d" % (session['name'], session['seats'], session['taken_seats'])
 
 # 3.create a new session
 course_id = call('openacademy.course', 'search', [('name','ilike','Functional')])[0]
-session_id = call(model, 'create', {
-    'name' : 'My session loca',
+#session_id = call(model, 'create', {
+#    'name' : 'My session loca',
+#    'course_id' : course_id,
+#})
+responsible_id = call('res.partner', 'search', [('name','=','Agrolait')])[0]
+new_session_id = call(model, 'create', {
+    'name' : 'Session from WS 2',
     'course_id' : course_id,
+    'instructor_id': responsible_id,
+    'attendee_ids': [(4,8),(4,3)],
 })
+print new_session_id
 
